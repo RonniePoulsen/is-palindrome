0c9fb97cab6a1619d3f64aa44a526b7d6ca32561
Ronnie Poulsen <ronron.dk@gmail.com>
2017-02-03 10:34:39 +0100

    Optimize code. Compare the whole string instead of splitting in two pieces.

--- IsPalindrome/Program.cs
+++ IsPalindrome/Program.cs
@@ -18,41 +18,14 @@ namespace IsPalindrome
             // Convert input to lowercase and remove whitespaces
             var inputToLowerWithNoWhiteSpace = input.ToLower().Replace(" ", "");
 
-            var firstHalf = "";
-            var secondHalf = "";
+            // Convert input to array, reverse and convert back to string
+            var arr = inputToLowerWithNoWhiteSpace.ToCharArray();
+            Array.Reverse(arr);
+            var reverseString = new string(arr);
+             
             
-            // Check if input length is an even number
-            if (inputToLowerWithNoWhiteSpace.Length % 2 == 0)
-            {
-                // Get first half of the original input
-                firstHalf = inputToLowerWithNoWhiteSpace.Substring(0, inputToLowerWithNoWhiteSpace.Length / 2);
-
-                // Convert input to an array and reverse
-                var arr = inputToLowerWithNoWhiteSpace.ToCharArray();
-                Array.Reverse(arr);
-                var reverseString = new string(arr);
-
-                // Get first half of the reversed string
-                secondHalf = reverseString.Substring(0, reverseString.Length / 2);
-            }
-            else
-            {
-                // Get first half (-1 character) of the original input
-                firstHalf = inputToLowerWithNoWhiteSpace.Substring(0, (inputToLowerWithNoWhiteSpace.Length / 2) - 1);
-
-                // Convert input to an array and reverse
-                var arr = inputToLowerWithNoWhiteSpace.ToCharArray();
-                Array.Reverse(arr);
-                var reverseString = new string(arr);
-
-                // Get first half (-1 character) of the reversed string
-                secondHalf = reverseString.Substring(0, (reverseString.Length / 2) - 1);
-            }
-
-
-
             // Write out result
-            if (firstHalf.Equals(secondHalf))
+            if (inputToLowerWithNoWhiteSpace.Equals(reverseString))
             {
                 Console.WriteLine("\"" + input + "\" is a palindrome.");
             }
